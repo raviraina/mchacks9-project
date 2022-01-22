b9c0c6fc01b69f1cbf99c5540bc4bee072788e38
Mari5498 <aryan-7@hotmail.com>
2022-01-22 19:35:11 +0400

    Modified Login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,10 +4,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <button onClick={() => loginWithRedirect()}>
-      Log in/Sign up
-    </button>
+    <div style={{ "text-align": "center" }} >
+      <button onClick={() => loginWithRedirect()}>
+        Log in/Sign up
+      </button>
+    </ div>
   )
 };
+
+
 
 export default Login;
