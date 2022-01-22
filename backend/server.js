fe3e1758e83c2b055d7596b9a22b5832a1931147
Ravi Raina <ravi.raina@brentwood.ca>
2022-01-21 22:00:00 -0500

    npm modules, base setup of express and socket

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+// init
+const app = require('express');
+const socket = require('socket.io');
+const randomstring = require('randomstring');
+const express = app();
+
+// start server on port 4000
+const server = express.listen(4000, () => {
+    console.log("server started at http://localhost:4000");
+})
+
+// init
+express.use(app.static('public'));
+const io = socket(server);
