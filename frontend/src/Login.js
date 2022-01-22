636b70bbdf6dca84a3a097ab82fb77248cc7abd8
Mari5498 <aryan-7@hotmail.com>
2022-01-22 14:40:52 +0400

    Changed Login.js to work with Auth0

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
-const Login = () => {
-  return <div>Login</div>;
+import { useAuth0 } from '@auth0/auth0-react'
+const LoginButton = () => {
+  const { loginwithRedirect } = useAuth0();
+  return (
+    <button onClick={() => loginwithRedirect()}>
+      Log in/Sign up
+    </button>
+  )
 };
 
-export default Login;
+export default LoginButton;
